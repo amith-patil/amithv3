3c4a1581df537a3a1decdc8b1f137ed87ea7707f
amith-patil <amithsp93@live.com>
2026-08-16 22:51:59 +0530

    feat: add homepage structure, global styles, and interactive career component with animations

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,8 +1,10 @@
 import { Component, HostListener } from '@angular/core';
+import { RevealDirective } from '../../directives/reveal';
+
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [RevealDirective],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
@@ -13,6 +15,6 @@ export class Hero {
   onWindowScroll() {
     // We update the scroll position. 
     // Using a factor (like 0.5) controls the speed of the parallax.
-    this.scrollY = window.scrollY * 0.75;
+    this.scrollY = window.scrollY * 0.5;
   }
 }
